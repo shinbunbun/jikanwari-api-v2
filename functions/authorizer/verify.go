81659baa80ebe9447fdf298cf21df8c7b2080aca
shinbunbun <takamuneyuto@gmail.com>
2022-07-31 09:01:01 +0000

    add verify client_id code

--- functions/authorizer/verify.go
+++ functions/authorizer/verify.go
@@ -1,24 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
 	"github.com/shinbunbun/jikanwari-api-v2/tools"
 )
 
+type VerifyApiResponse struct {
+	ClientId  string `json:"client_id"`
+	Scope     string `json:"scope"`
+	ExpiresIn int    `json:"expires_in"`
+}
+
 func getTokenFromAuthZHeader(authZHeader string) string {
 	return strings.Split(authZHeader, "Bearer ")[1]
 }
 
-func verify(authZHeader string) error {
+func parseVerifyApiResponse(resp string) (*VerifyApiResponse, error) {
+	var parsedResp = new(VerifyApiResponse)
+	err := json.Unmarshal([]byte(resp), &parsedResp)
+	if err != nil {
+		return &VerifyApiResponse{}, fmt.Errorf("JSON unmarshal error: %v", err.Error())
+	}
+	return parsedResp, nil
+}
+
+func verifyClientId(config tools.Config, parsedResp *VerifyApiResponse) error {
+	if parsedResp.ClientId != config.GetEnv("LINE_CHANNEL_ID") {
+		return fmt.Errorf("Invalid client id: %v", parsedResp.ClientId)
+	}
+	return nil
+}
+
+func verify(config tools.Config, authZHeader string) error {
 
 	idToken := getTokenFromAuthZHeader(authZHeader)
 
-	_, err := tools.GetRequest("https://api.line.me/oauth2/v2.1/verify?access_token="+idToken, nil)
+	resp, err := tools.GetRequest("https://api.line.me/oauth2/v2.1/verify?access_token="+idToken, nil)
 	if err != nil {
 		return fmt.Errorf("Invalid token: %v", err.Error())
 	}
 
-	return nil
+	parsedResp, err := parseVerifyApiResponse(resp)
+	if err != nil {
+		return err
+	}
+
+	return verifyClientId(config, parsedResp)
 }
