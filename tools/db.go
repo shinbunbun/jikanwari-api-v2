805f7f42006fc6e133fd500e4b0e00f401f05a06
shinbunbun <takamuneyuto@gmail.com>
2022-08-01 01:12:20 +0000

    add GetByID

--- tools/db.go
+++ tools/db.go
@@ -1,10 +1,33 @@
 package tools
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/guregu/dynamo"
+)
+
+type DynamoItem struct {
+	ID          string
+	Date        string `dynamo:"date"`
+	Flag        string `dynamo:"flag,omitempty"`
+	Fri         string `dynamo:"fri"`
+	Mon         string `dynamo:"mon"`
+	NotifyToken string `dynamo:"notifyToken,omitempty"`
+	Property    string `dynamo:"property,omitempty"`
+	Sat         string `dynamo:"sat"`
+	Thu         string `dynamo:"thu"`
+	Tue         string `dynamo:"tue"`
+	Uuid        string `dynamo:"uuid,omitempty"`
+	Wed         string `dynamo:"wed"`
+}
+
+const (
+	DBName   = "TimeTable"
+	Region   = "ap-north-east-1"
+	Endpoint = "http://dynamodb-local:8000"
 )
 
 func createSession() *session.Session {
@@ -17,12 +40,12 @@ func (*Local) GetDynamoDB() dynamo.Table {
 	ses := createSession()
 
 	db = dynamo.New(ses, &aws.Config{
-		Region:      aws.String("ap-north-east-1"),
-		Endpoint:    aws.String("http://dynamodb-local:8000"),
+		Region:      aws.String(Region),
+		Endpoint:    aws.String(Endpoint),
 		DisableSSL:  aws.Bool(true),
 		Credentials: credentials.NewStaticCredentials("dummy", "dummy", "dummy"),
 	})
-	return db.Table("UBIC-FOOD")
+	return db.Table(DBName)
 }
 
 func (*Prod) GetDynamoDB() dynamo.Table {
@@ -32,5 +55,15 @@ func (*Prod) GetDynamoDB() dynamo.Table {
 
 	db = dynamo.New(ses)
 
-	return db.Table("UBIC-FOOD")
+	return db.Table(DBName)
+}
+
+func GetByID(id string, table *dynamo.Table) (*DynamoItem, error) {
+	var readResult DynamoItem
+	err := table.Get("ID", id).One(&readResult)
+	if err != nil {
+		fmt.Printf("Failed to get item[%v]\n", err)
+		return &DynamoItem{}, err
+	}
+	return &readResult, nil
 }
---
+++ tools/db_test.go
@@ -0,0 +1,57 @@
+package tools
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/guregu/dynamo"
+)
+
+func TestGetByIDDataType(t *testing.T) {
+	local := Local{}
+	var config Config = &local
+	table := config.GetDynamoDB()
+
+	type args struct {
+		id    string
+		table *dynamo.Table
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *DynamoItem
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				id:    "test_user",
+				table: &table,
+			},
+			want: &DynamoItem{
+				ID:   "test_user",
+				Date: "2022年07月30日 (土) 0時0分0秒",
+				Flag: "25",
+				Fri:  "friday",
+				Mon:  "monday",
+				Sat:  "saturday",
+				Thu:  "thursday",
+				Tue:  "tuesday",
+				Uuid: "test_id",
+				Wed:  "wednesday",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetByID(tt.args.id, tt.args.table)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetByIDDataType() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetByIDDataType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
